95d64e41f66c9b1b190fc055fc203b9f19db53af
wherculano <wagherculano@hotmail.com>
2019-03-08 23:21:41 -0300

    1016 resolvido

---
+++ 1016 - Distância.py
@@ -0,0 +1,26 @@
+'''
+Dois carros (X e Y) partem em uma mesma direção.
+O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
+
+Em uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X, ou seja,
+consegue se afastar um quilômetro a cada 2 minutos.
+
+Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.
+Entrada
+O arquivo de entrada contém um número inteiro.
+
+Saída
+Imprima o tempo necessário seguido da mensagem "minutos".
+    >>> 30
+    60 minutos
+
+    >>> 110
+    220 minutos
+
+    >>> 7
+    14 minutos
+'''
+
+X = int(input())
+temp = 2*X
+print('{} minutos'.format(temp))
