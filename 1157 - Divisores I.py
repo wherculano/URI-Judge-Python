10c55566db938e56ffc839e3a73f1481fce72811
wagherculano <wagherculano@hotmail.com>
2021-03-24 21:54:45 -0300

    1157 - Divisores I

---
+++ 1157 - Divisores I.py
@@ -0,0 +1,31 @@
+"""
+Ler um número inteiro N e calcular todos os seus divisores.
+
+Entrada
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+Escreva todos os divisores positivos de N, um valor por linha.
+
+Exemplo de Entrada
+6
+
+Exemplo de Saída
+1
+2
+3
+6
+"""
+
+
+def divisores(n):
+    """
+    >>> divisores(6)
+    1
+    2
+    3
+    6
+    """
+    for i in range(1, n+1):
+        if 6 % i == 0:
+            print(i)
